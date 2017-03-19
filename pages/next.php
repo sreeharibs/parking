38cd81c713ba6bcd4ed835205850e188e3f7e00f
Sreehari B S <sreeharibs95@gmail.com>
2017-03-19 21:50:06 +0530

    Updated pages blank.php and next.php with sample test fields

---
+++ pages/next.php
@@ -0,0 +1,25 @@
+<?php include('../include/header.php'); ?>
+
+        <!-- Page Content -->
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">SPARK</h1>
+                    </div>
+                    <!-- /.col-lg-12 -->
+			<form role="form" name="form1" action="finish.php" method="post">
+
+				<img class="img-responsive" width="50%" src="../images/lot1.jpg"></img>
+
+				<button type="submit" class="btn btn-default">Done</button>
+			</form>
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+        <!-- /#page-wrapper -->
+
+    
+<?php include('../include/footer.php'); ?>
