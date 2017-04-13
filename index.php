05b7ea9865eb3203477434856432f78127cb2f16
Sreehari B S <sreeharibs95@gmail.com>
2017-04-13 11:34:42 +0530

    Almost complete.

--- index.php
+++ index.php
@@ -8,20 +8,20 @@
                         <h1 class="page-header">SPARK</h1>
                     </div>
                     <!-- /.col-lg-12 -->
-			<form role="form" name="form1" action="next.php" method="post">
+			<form role="form" name="form1" action="park.php" method="post">
 				<div class="form-group">
                                             <label>Name</label>
-                                            <input class="form-control">
+                                            <input class="form-control" name="name">
                                             <p class="help-block">Your Name goes here</p>
                                 </div>
 				<div class="form-group">
                                             <label>Email</label>
-                                            <input class="form-control">
+                                            <input class="form-control" name="email">
                                             <p class="help-block">Your Email goes here</p>
                                 </div>
 				<div class="form-group">
                                             <label>Vehicle Reg No</label>
-                                            <input class="form-control">
+                                            <input class="form-control" name="vehicleno">
                                             <p class="help-block">Registration Number of vehicle</p>
                                 </div>
 				<button type="submit" class="btn btn-default">Next</button>
--- mycar.php
+++ mycar.php
@@ -8,15 +8,22 @@
                         <h1 class="page-header">SPARK</h1>
                     </div>
                     <!-- /.col-lg-12 -->
-			<form role="form" name="form1" action="finish.php" method="post">
+			<form role="form" name="form1" action="unpark.php" method="post">
+<input type="hidden" name="lotno" value="<?php echo $_GET['lot'] ?>"/>
 				<div class="col-lg-3"></div>
 				<div class="col-lg-6">
-					<img class="img-responsive" width="100%" src="<?php echo "images/lot".$_GET['lot'].".jpg" ?>"></img>
+					<img class="img-responsive" width="100%" src="<?php echo "images/car".$_GET['lot'].".jpg" ?>"></img>
+					<div class="col-lg-12">
+
+						<div class="col-lg-4"></div>
+	
+						<button type="submit" class="col-lg-4 btn btn-primary btn-lg btn-block">UnPark</button>
+					</div>
 				</div>
 				<div class="col-lg-3"></div>
 				
 				<div class="col-lg-12">
-					<button type="submit" class="btn btn-default">Done</button>
+					
 				</div>
 			</form>
                 </div>
