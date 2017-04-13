eaf9d5cf41a53b63194f08d238e2f3b33b4d4e74
Sreehari B S <sreeharibs95@gmail.com>
2017-04-13 11:45:07 +0530

    Some small edits

--- next.php
+++ next.php
@@ -8,7 +8,7 @@
                         <h1 class="page-header">SPARK</h1>
                     </div>
                     <!-- /.col-lg-12 -->
-			<form role="form" name="form1" action="finish.php" method="post">
+			<form role="form" name="form1" action="index.php" method="post">
 				<div class="col-lg-3"></div>
 				<div class="col-lg-6">
 					<img class="img-responsive" width="100%" src="<?php echo "images/lot".$_GET['lot'].".jpg" ?>"></img>
